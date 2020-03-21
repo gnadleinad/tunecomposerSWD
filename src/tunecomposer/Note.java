7d48c8a4b9b8f42314db16584d01def2acdc07c6
“hillzp” <zoe.hill1999@gmail.com>
2020-03-20 19:15:50 -1000

    updated data structures

--- src/tunecomposer/Note.java
+++ src/tunecomposer/Note.java
@@ -15,29 +15,37 @@ public class Note {
 
     boolean isSelected = false;
     private String instrument;
-    private Rectangle display_note;
+    public Double x;
+    public Double y; //midi value of y
+    public Double midi_y; 
+    public Rectangle display_note;
     
     /**
      * Constructs a note. 
      * @param temp_instrument 
      */
-    public Note(String temp_instrument){
+    public Note(Double temp_x,Double temp_y, String temp_instrument){
         instrument = temp_instrument;
         
+        x = temp_x;
+        y = temp_y;
+        midi_y = Math.floor(127-((temp_y - 30) / 10));
+        draw_note(x,y);
+        
     }
+    
     /**
      * makes the rectangle on screen symbolizing a note
      * @param x the x coordinate of the note
      * @param y the y coordinate of the note
      * @return display_note
      */
-    public Rectangle draw_note(double x, double y) {
+    public void draw_note(double x, double y) {
         y = Math.floor(y / 10) * 10;
         display_note = new Rectangle(x, y, 100, 10);
         display_note.getStyleClass().add("note");
         Convert_Instrument();
         display_note.getStyleClass().add(instrument);
-     return display_note;
     }
     
     public void display_delete(){
@@ -54,6 +62,7 @@ public class Note {
             instrument = instrument.replace(' ', '-');
         }
     }
+    
     
     
     
