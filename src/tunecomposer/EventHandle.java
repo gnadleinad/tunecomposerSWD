50a6749eb4b23c94f491e27ac80390c166895fb5
“hillzp” <zoe.hill1999@gmail.com>
2020-04-23 11:37:57 -0700

    controller consolidation

--- src/tunecomposer/EventHandle.java
+++ src/tunecomposer/EventHandle.java
@@ -5,18 +5,30 @@
  */
 package tunecomposer;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import static javafx.scene.paint.Color.BLACK;
 import static javafx.scene.paint.Color.TRANSPARENT;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import javafx.util.Pair;
-import static tunecomposer.TuneComposer.current_instrument;
 
 /**
  *
@@ -40,18 +52,245 @@ public class EventHandle {
     public static Note dragged;
     public static double starting_point_x;
     public static double starting_point_y;
+    
+    //START OF BAD STUFF
+    /**
+     * Represents the number of pitch steps for 
+     * do, re, mi, fa, so, la, ti, do.
+     * Source: https://en.wikipedia.org/wiki/Solfège
+     */
+    private static final int[] SCALE = {0, 2, 4, 5, 7, 9, 11, 12};
+    
+    /**
+     * Play notes at maximum volume.
+     */
+    
+    private static final int VOLUME = 127;
+    
+    /**
+     * One midi player is used throughout, so we can stop a scale that is
+     * currently playing.
+     */
+    private final MidiPlayer player;
+    
+    /**
+     * Represents the time position of notes along the width as keys
+     * Represents the pitch position of notes along the height as values
+     * player object will play given pitch when time has passed the position.
+     */
 
+    //private static Map<Pair, Note> notePosition;
+    
+
+    private static TreeMap<Pair,Note> noteTreeMap;
+    
+    //private ArrayList<Note> selected;
+    
+     /**
+     * ArrayList of integer lists that stores the MIDI event parameters
+     * for the addMidiEvent method
+     */
+    private static ArrayList<int[]> MIDI_events;
+    
+    @FXML
+    private Line one_line;
+    
+    @FXML
+    private ToggleGroup instrument;
+    
+    @FXML Line red_line;
+    
+    @FXML
+    public AnchorPane lines_pane;
+    
+    @FXML
+    public AnchorPane notes_pane;
+    
+    @FXML
+    public AnchorPane redline_pane;
+    
+    public static String current_instrument;
+  
+    public TranslateTransition transition;
+
+    private static double finalNote;
+    
+    public EventHandle() {
+        this.player = new MidiPlayer(1,10000);
+        this.MIDI_events = new ArrayList<>();
+        //this.notePosition = new HashMap<>();
+        //this.selected = new ArrayList<>();
+        this.transition = new TranslateTransition();
+        //this.finalNote = 0.0;
+        
+        this.noteTreeMap = new TreeMap<>(new PairComparator());
+        
+
+    }
     
     
+      
+     /**
+     * Sorts an ArrayList of list of integers given an element to compare.
+     * @param unsorted the ArrayList with which to sort
+     * @param element the element within all integer lists that will be compared
+     */
+    
+    public void sortArrayList(ArrayList<int[]> unsorted, int element) {
+        Collections.sort(unsorted, new Comparator<int[]>() {
+            public int compare(int[] current_int, int[] other_int) {
+                return Integer.compare(current_int[element],other_int[element]);
+            }
+        });
+    }
+    
+
+    /**
+     * Adds all of the MIDI_events to the current composition.
+     */
+    protected void addAllEvents() {
+        for  (int[] event : MIDI_events) {
+            player.addMidiEvent(event[0], event[1], event[2], event[3], event[4]);
+        }
+    }
+    
+    /**
+     * Play a new scale, after stopping and clearing any previous scale.
+     */
     
     
-        //controller.one_Line();
+    protected void playScale() {
+        /*
+        int channel_accum;
+        player.stop();
+        player.clear();
+
+        sortArrayList(MIDI_events, 3);
+        addAllEvents();
+        channel_accum = 0;
+        for(Map.Entry<Pair, Note> entry : EventHandle.notePosition.entrySet()){ 
+           player.addNote((int)Math.round((double)(entry.getValue()).midi_y), VOLUME, (int)Math.round((double)(entry.getValue()).x), (int)Math.round((double)(entry.getValue()).duration), MIDI_events.get(channel_accum)[0] - ShortMessage.PROGRAM_CHANGE, 0);       
+
+          channel_accum += 1;
+        } 
+
+        player.play();
+        */
+    }
         
     
-    public EventHandle(TuneComposer c) {
+    /**
+     * When the user clicks the "Play scale" button, show a dialog to get the 
+     * starting note and then play the scale.
+     * @param event the button click event
+     */
+    @FXML 
+    protected void handlePlayScaleButtonAction(ActionEvent event) {
+        transition.stop();
+        move_red();
+        playScale();
+        
+    } 
+    
+    /**
+     * When the user clicks the "Stop playing" button, stop playing the scale.
+     * @param event the button click event
+     */
+    @FXML 
+    protected void handleStopPlayingButtonAction(ActionEvent event) {
+        player.stop();
+        //timeline.jumpTo(Duration.INDEFINITE); //Possibly not necessary line
+        transition.stop();
+    }  
+    
+    @FXML
+
+    protected void handleDeleteButtonAction(ActionEvent event) throws InvocationTargetException{
+//        Set<Pair> set = new HashSet<> ();
+//        for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
+////            if (entry.getValue().isSelected){
+////                entry.getValue().display_delete();
+////                set.add(entry.getKey());
+//                //notePosition.remove(entry.getKey());
+//
+//            //}
+//            
+//        }
+//        
+//        notePosition.keySet().removeAll(set);
+//        
+//        finalNote = 0.0;
+//        //notePosition.clear(); //deletes note positions that are used to create player composition.
+//        //MIDI_events.clear();
+//        double current_end = 0.0;
+//        for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
+//            current_end = (double)(entry.getKey()).getKey()+(entry.getValue()).duration;
+//            if(current_end > finalNote){
+//                finalNote = current_end;
+//            }      
+//        }
+//
+//        
+//
+  }
+
+    @FXML
+    protected void handleSelectAllButtonAction(ActionEvent event){
+//        selected.clear();
+//        for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
+//            entry.getValue().display_select();
+//            selected.add(entry.getValue());
+//
+//        } 
+    }
+    
+    /**
+     * When the user clicks the "Exit" menu item, exit the program.
+     * @param event the menu selection event
+     */
+    @FXML
+    protected void handleExitMenuItemAction(ActionEvent event) {
+        System.exit(0);
+    }
+    /**
+     * Constructs a line graphic and duplicates until window is filled.
+     */
+    @FXML
+    public void one_Line()  {
+     double y = 0;
+     while (y < 1280){
+         Line line = new Line(0,y,2000, y);
+         redline_pane.getChildren().add(line);
+         y = y + 10;
+        }
+    }
+    
+    public void change_instrument(){
+        RadioButton selectedRadioButton = (RadioButton) instrument.getSelectedToggle();
+        String toggleGroupValue = selectedRadioButton.getText();
+        current_instrument = toggleGroupValue;    
     }
 
-    static public void onClick(MouseEvent event, TuneComposer tc){
+    /**
+     * Creates and moves a red line across the screen to show the duration of time.
+     * @param finalNote the x time position of the last note
+     */
+    public void move_red() {
+        double duration = finalNote * 6 + 150;
+        transition.setDuration(Duration.millis(duration));
+        transition.setNode(red_line);
+        transition.setFromX(red_line.getStartX()+22);
+        if(finalNote == 0){
+            transition.setToX(finalNote);
+        }
+        else{transition.setToX(finalNote);}
+        transition.setInterpolator(Interpolator.LINEAR);
+        red_line.setOpacity(1);
+        transition.play();
+    }
+ 
+    // GOOD STUFF STARTS HERE
+    public void onClick(MouseEvent event){
         
         double x  = event.getX();
         double y  = event.getY();
@@ -67,15 +306,11 @@ public class EventHandle {
                 }  
             }
 
-            makeNote(event,x,y,tc);
-        }
-        
-
-        
-        
+            makeNote(event,x,y);
+        }   
     }
       
-    static public void onPressed(MouseEvent event, TuneComposer tc) { 
+    public void onPressed(MouseEvent event) { 
         inside_rect = false;
         starting_point_x = event.getX() ;
         starting_point_y = event.getY() ;
@@ -86,13 +321,13 @@ public class EventHandle {
             select_rect = new Rectangle() ;
             select_rect.getStyleClass().add("selectionRect");
             // A non-finished rectangle has always the same color.
-            tc.notes_pane.getChildren().add(select_rect);
+            notes_pane.getChildren().add(select_rect);
             new_rectangle_is_being_drawn = true ;
         }
 
     }
     
-    static public void onDragged(MouseEvent event, TuneComposer tc) {  
+    public void onDragged(MouseEvent event) {  
 
         if(extend == true || new_rectangle_is_being_drawn == true){
             drag = false;
@@ -109,8 +344,7 @@ public class EventHandle {
         }
           
         if (extend == true){
-            extendNotes(selected, current_ending_point_x);
-            
+            extendNotes(selected, current_ending_point_x);   
         }
          
         if ( new_rectangle_is_being_drawn == true ){
@@ -122,18 +356,18 @@ public class EventHandle {
         }
     }
     
-    static public void onReleased(MouseEvent event, TuneComposer tc) {
+    public void onReleased(MouseEvent event) {
         double ending_point_x = event.getX();
         double ending_point_y = event.getY();
         
         if ( new_rectangle_is_being_drawn == true ){
-            endDrawingRectangle(event, ending_point_x,ending_point_y, tc);
+            endDrawingRectangle(event, ending_point_x,ending_point_y);
         }
         if (drag == true){
-            endDrag(ending_point_x,ending_point_y, tc);
+            endDrag(ending_point_x,ending_point_y);
         }
         if (extend == true) {
-            endExtend(ending_point_x, tc);
+            endExtend(ending_point_x);
         }
         else{
             for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
@@ -149,9 +383,9 @@ public class EventHandle {
     
     
     
-    static public void makeNote(MouseEvent event, double x,double y, TuneComposer tc){
+    public void makeNote(MouseEvent event, double x,double y){
         deselectNotes(event);
-        //controller.change_instrument();
+        //change_instrument();
 
         Pair cordinates = new Pair(x,y);
         Note n = new Note(x,y,current_instrument);
@@ -159,7 +393,7 @@ public class EventHandle {
 
 
         selected.add(n);
-        tc.notes_pane.getChildren().add(n.display_note);
+        notes_pane.getChildren().add(n.display_note);
         n.display_select();
         if(n.midi_y >= 0 && n.midi_y < 128){
             notePosition.put(cordinates,n);
@@ -185,7 +419,7 @@ public class EventHandle {
         }
     }
     
-    public static void selectNotes(MouseEvent event){
+    public void selectNotes(MouseEvent event){
         for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
             if(entry.getValue().display_note.contains(starting_point_x, starting_point_y)){
                 dragged = entry.getValue();
@@ -201,7 +435,7 @@ public class EventHandle {
         }
     }
     
-    static public void selectNote(MouseEvent event, Note n, ArrayList<Note> array){
+    public void selectNote(MouseEvent event, Note n, ArrayList<Note> array){
         if(!selected.contains(n)){
             n.display_select();
             selected.add(n); 
@@ -209,7 +443,7 @@ public class EventHandle {
 
     }
     
-    public static void deselectNotes(MouseEvent event){
+    public void deselectNotes(MouseEvent event){
         if(event.isControlDown() == false){
             for (Note note : selected){
                 note.display_deselect();
@@ -219,12 +453,17 @@ public class EventHandle {
         
     }
     
-    public static void deselectNote(Note n){
+    public void deselectNote(Note n){
         n.display_deselect();
         selected.remove(n);
+        //REMOVE
+        //System.out.println(notes_pane.equals(tcx.notes_pane));
+        //System.out.println(tcx.notes_pane.getChildren().size());
+        //notes_pane.getChildren().remove(n.display_note);
+        //System.out.println(notes_pane.getChildren().size());
     }
     
-    public static void dragNotes(ArrayList<Note> array, double x, double y){
+    public void dragNotes(ArrayList<Note> array, double x, double y){
         double dify = (y - dragged.y);
         double difx = (x - dragged.x);
         for (Note note : selected) {
@@ -234,7 +473,7 @@ public class EventHandle {
         } 
     }
     
-    public static void extendNotes(ArrayList<Note> array, double x){
+    public void extendNotes(ArrayList<Note> array, double x){
         double extentionlen = (x - dragged.x);
         for (Note note : array) {
             if(extentionlen < 5.0){
@@ -245,7 +484,7 @@ public class EventHandle {
     }
     
     
-    public static void endDrawingRectangle(MouseEvent event, double x, double y, TuneComposer tc){
+    public void endDrawingRectangle(MouseEvent event, double x, double y){
         deselectNotes(event);                
         for(Map.Entry<Pair, Note> entry : notePosition.entrySet()){ 
            if ((entry.getValue().display_note.getY() > Math.min(starting_point_y,y)  && entry.getValue().display_note.getY() < Math.max(y,starting_point_y))
@@ -262,12 +501,12 @@ public class EventHandle {
         }
 
 
-       tc.notes_pane.getChildren().remove( select_rect ) ;
+       notes_pane.getChildren().remove( select_rect ) ;
        new_rectangle_is_being_drawn = false ;
         
     }
     
-    public static void endDrag(double x, double y, TuneComposer tc) {
+    public void endDrag(double x, double y) {
         double dify = (y - dragged.y);
         double difx = (x - dragged.x);
 
@@ -289,14 +528,14 @@ public class EventHandle {
         }
     }
     
-    public static void resetBooleans(){
+    public void resetBooleans(){
         drag = false;
         extend = false;
         inside_rect = false;
         new_rectangle_is_being_drawn = false;     
     }
     
-    public static void endExtend(double x, TuneComposer tc) {
+    public void endExtend(double x) {
         double ext_len = (x - dragged.x);
         for (Note note : selected) {
             Pair cordinate = new Pair(note.x,note.y);
@@ -308,7 +547,7 @@ public class EventHandle {
         }
     }
     
-    private static void controlClick(MouseEvent event, Note note, double x, double y){
+    private void controlClick(MouseEvent event, Note note, double x, double y){
         if(!selected.contains(note)){       
             selectNote(event, note, selected);
         }
@@ -317,7 +556,7 @@ public class EventHandle {
         }
     }
     
-    public static void adjust_rectangle_properties( double starting_point_x,
+    public void adjust_rectangle_properties( double starting_point_x,
                                      double starting_point_y,
                                      double ending_point_x,
                                      double ending_point_y,
@@ -340,4 +579,5 @@ public class EventHandle {
          given_rectangle.setY( given_rectangle.getY() - given_rectangle.getHeight() ) ;
       }
    }
+   //GOOD STUFF ENDS HERE
 }   
