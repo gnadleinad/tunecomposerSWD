e3075f61e3af922afcd89415891faae06b26187f
“hillzp” <zoe.hill1999@gmail.com>
2020-04-24 12:51:25 -0700

    Group

---
+++ src/tunecomposer/Group.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunecomposer;
+
+import java.util.ArrayList;
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author zoehill
+ */
+public class Group extends Rectangle implements Moveable{
+    public Double x;
+    public Double y;
+    private static ArrayList<Moveable> group = new ArrayList<>();
+    
+    public Group(ArrayList selected){
+        group = selected;
+        x = 0.0;
+        y = 0.0;
+        
+        
+        
+    }
+    
+    private void setXandY(){
+        /*
+        for(Moveable mov: group){
+            if(mov.getMoveableX() < x ){
+            }
+        }
+        */
+    }
+    
+    public double getMoveableX(){return 0.0;}
+    
+    public double getMoveableY(){return 0.0;}
+    
+    public void drag(double difx, double dify){}
+    
+    public void releaseDrag(double difx, double dify){}
+    
+    public void extend(double extentionlen){}
+    
+    public void releaseExtend(double extentionlen){}
+    
+    public void display_select(){
+        this.getStyleClass().add("group");
+    }
+
+    public void display_deselect(){
+        this.getStyleClass().remove("group");
+    }
+    
+}
