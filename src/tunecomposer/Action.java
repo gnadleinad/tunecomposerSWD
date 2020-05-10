e428b3c96450dbe53bf08e0bc2a453444eb1f794
“hillzp” <zoe.hill1999@gmail.com>
2020-05-10 13:38:58 -0700

    action interface

---
+++ src/tunecomposer/Action.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunecomposer;
+
+/**
+ *
+ * @author zoehill
+ */
+public interface Action {
+    
+    public void doAction();
+    
+    public void undoAction();
+    
+}
