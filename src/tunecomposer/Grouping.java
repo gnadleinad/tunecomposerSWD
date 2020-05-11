16e1187fd8e4d84a8cf1839e2f1d1c636c97dbcb
“hillzp” <zoe.hill1999@gmail.com>
2020-05-10 20:52:41 -0700

    grouping action

---
+++ src/tunecomposer/Grouping.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunecomposer;
+
+import java.util.ArrayList;
+import tunecomposer.controllers.MainController;
+
+/**
+ *
+ * @author zoehill
+ */
+public class Grouping implements Action{
+    private MainController main;
+    
+    //private AddNote current;
+    /*
+    private MouseEvent event;
+    private double note_x;
+    private double note_y;
+    */
+    private ArrayList<Moveable> original_selected;
+    
+    private Group original_group;
+    
+    public Grouping(Group g, MainController m){
+
+        main = m;
+        original_group = g;
+        
+        redoAction();
+
+        main.done.push(this);
+    }
+    
+    @Override
+    public void redoAction() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Moveable> selected = main.getSelected();
+        main.getPaneChildren("notes_pane").add(original_group);
+        selected.clear();
+        selected.add(original_group);
+    }
+
+    @Override
+    public void undoAction() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Moveable> selected = main.getSelected();
+        main.removePaneChild("notes_pane",original_group);
+        for(Moveable item : original_group.group){
+            selected.add((Moveable)item);
+        } 
+        selected.remove(original_group);
+    }
+    
+}
